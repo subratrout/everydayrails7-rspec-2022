53227d841699ef89eadc6d8aeba0c4c8a7b1ac39
Subrat Rout <subratnrout@gmail.com>
2022-02-01 15:43:53 -0600

    Depend on Capybara Accessible Selectors

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,3 +3,7 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
+
+Capybara.configure do |config|
+  config.default_normalize_ws = true
+end
